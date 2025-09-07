89ef0890a50c57aa41248436970223f15c2d248f
宋九傲 <jiuaosong@BP.local>
2025-09-07 17:08:37 -0400

    practice stack design

--- src/Design/stack.cpp
+++ src/Design/stack.cpp
@@ -1,57 +1,51 @@
 #include <iostream>
 using namespace std;
-
-class Stack
-{
+class Stack {
 public:
-    Stack(int capacity) : topindex(-1), capacity(capacity)
-    {
-        stack = (int *)malloc(sizeof(int) * capacity);
-        if (!stack)
-        {
-            cout << "malloc failed" << endl;
+    Stack(int capacity):capacity(capacity), topIndex(-1){
+        stack = new int[capacity];
+        if(!stack) {
+            cout << "allocate memory failed!" << endl;
             exit(1);
         }
     }
-    ~Stack()
-    {
-        free(stack);
+    ~Stack() {
+        delete []stack;
     }
-    void push(int data)
-    {
-        if (topindex == capacity - 1)
-        {
-            cout << "stack is full" << endl;
-            return;
+    void push(int data) {
+        if(topIndex == capacity - 1) {
+            cout << "stack is full!" << endl;
         }
-        topindex++;
-        stack[topindex] = data;
+        topIndex++;
+        stack[topIndex] = data;
     }
-    int pop()
-    {
-        if (topindex == -1)
-        {
-            cout << "stack is empty" << endl;
-            return;
+    int pop() {
+        if(topIndex == -1) {
+            cout<<"stack is empty!"<<endl;
+            return -1;
         }
-        return stack[topindex--];
+        return stack[topIndex--];
     }
-    int top() const
-    {
-        if (topindex == -1)
-        {
-            cout << "stack is empty" << endl;
-            return;
+    const int top() {
+        if(topIndex == -1) {
+            cout << "stack is empty!" << endl;
+            return -1;
         }
-        return stack[topindex];
+        return stack[topIndex];
     }
-    bool isEmpty() const
-    {
-        return topindex == -1;
+    bool isEmpty() {
+        return topIndex == -1;
     }
-
 private:
-    int *stack;
-    int topindex;
+    int* stack;
     int capacity;
+    int topIndex;
 };
+
+int main() {
+    Stack stk(10);
+    stk.pop();
+    stk.push(1);
+    stk.isEmpty();
+    stk.top();
+}
